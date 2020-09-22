241fe6ee35bfef3c2cfc89698702c152a90f894a
Lukas Saylor <slappyhoursound@gmail.com>
2020-09-22 15:28:31 -0400

    collapsible tabs added

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
@@ -9,15 +9,17 @@ import { Controlled as ControlledEditor } from "react-codemirror2";
 export default function Editor(props) {
   const { language, displayName, value, onChange } = props;
 
-    function handleChange(editor, data, value) {
-        onChange(value)
-    }
+  const [open, setOpen] = useState(true);
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button onClick={() => setOpen(prevOpen => !prevOpen)}>O/C</button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
@@ -27,7 +29,7 @@ export default function Editor(props) {
           lineWrapping: true,
           lint: true,
           mode: language,
-          theme: 'material',
+          theme: "material",
           lineNumbers: true,
         }}
       />
