94301a6fe1796c6209da0c3d6e9c0dccdabc2243
Lukas Saylor <slappyhoursound@gmail.com>
2020-09-22 12:54:33 -0400

    editor components

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Editor from "./Editor";
+
+function App() {
+  const [html, setHtml] = useState('')
+
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml} />
+        <Editor />
+        <Editor />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
