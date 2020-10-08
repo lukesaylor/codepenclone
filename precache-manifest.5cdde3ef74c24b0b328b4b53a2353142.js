3ca9c75905d9a13658e74ead4c0599d01e611fac
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-08 14:32:25 -0400

    Updates

---
+++ precache-manifest.5cdde3ef74c24b0b328b4b53a2353142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c395809959436e4282d5b59e12cd6cb5",
+    "url": "/codepenclone/index.html"
+  },
+  {
+    "revision": "8de893e162ab1a5b6729",
+    "url": "/codepenclone/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "e76c2103233af597e852",
+    "url": "/codepenclone/static/css/main.8f6bc4bc.chunk.css"
+  },
+  {
+    "revision": "8de893e162ab1a5b6729",
+    "url": "/codepenclone/static/js/2.7009e1c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepenclone/static/js/2.7009e1c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76c2103233af597e852",
+    "url": "/codepenclone/static/js/main.da61b25a.chunk.js"
+  },
+  {
+    "revision": "5bb576c151cb9f0b2010",
+    "url": "/codepenclone/static/js/runtime-main.0594e244.js"
+  }
+]);
